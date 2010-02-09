ea48cb6b4710389e6987d5979ee5d94bf38fde90
martin.badenas <martin.badenas@gmail.com>
2010-02-09 18:53:40 +0000

    No puedes decir que no hago nada ;P

--- tftp_packet.c
+++ tftp_packet.c
@@ -13,33 +13,34 @@ int guess_packet_type(char *buff, int bufflen, packet_type *type) {
 
 // reads "buff" and inits "packet"
 int buff_to_packet_read_write(char *buff, int bufflen, packet_read_write *packet) {
-	int strLen, strModeLen;
+	int strLen, posicion;
 	
-	strLen = 2;
 	packet->op = (short) buff[1];
 	// el op de los paquetes write y read es 1 y 2
 	if(packet->op != 1 && packet->op != 2) {
 		log_error("Invalid packet type!");
 		return -1;
 	}
-	while(buff[strLen] != '\0' && strLen < bufflen) {
-		strLen++;
-	}
-	if(buff[strLen] != '\0') {
-		log_error("Invalid buffer!");
-		return -1;
-	}
-	substr(packet->filename, buff, 2, strLen);
-	strModeLen = strLen + 2;
+	posicion = 2;//posicion en la que busca el nombre del archivo
 	strLen = 0;
-	while((buff[strModeLen + strLen] != '\0') && (strModeLen + strLen < bufflen)) {
+	while(buff[posicion + strLen] != '\0' && (posicion + strLen) < bufflen) {
 		strLen++;
 	}
-	if(buff[strModeLen + strLen] != '\0') {
+	if(buff[posicion + strLen] != '\0') {
 		log_error("Invalid buffer!");
 		return -1;
 	}
-	substr(packet->mode, buff, strModeLen, strLen);
+	substr(packet->filename, buff, posicion, strLen);
+	posicion += strLen + 1;//posicion en la que busca el modo
+	strLen = 0;
+	while((buff[posicion + strLen] != '\0') && (posicion + strLen < bufflen)) {
+		strLen++;
+	}
+	if(buff[posicion + strLen] != '\0') {
+		log_error("Invalid buffer!");
+		return -1;
+	}
+	substr(packet->mode, buff, posicion, strLen);
 	return 0;
 }
 
@@ -66,6 +67,16 @@ int buff_to_packet_data(char *buff, int bufflen, packet_data *packet) {
 }
 
 int buff_to_packet_ack(char *buff, int bufflen, packet_ack *packet) {
+	if(4 > bufflen) {
+		log_error("Buffer too short!");
+		return -1;
+	}
+	else if(4 < bufflen) {
+		log_error("Buffer too long!");
+		return -1;
+	}
+	packet->op = (short) buff[1];
+	packet->block = ((((short) buff[2]) * 10) + ((short) buff[3]));
 	return 0;
 }
 
