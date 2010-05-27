4002d3851b6d061ab64e732eee04cb2886f762b5
dhernandez0 <dhernandez0@users.noreply.github.com>
2010-05-27 19:49:16 +0000

    - GPL License added to files - README file added

--- test/test_tftp_io.c
+++ test/test_tftp_io.c
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2010 Dani Hernández Juárez, dhernandez0@gmail.com
+ * Copyright (C) 2010 Martín Badenas Beltrán, martin.badenas@gmail.com
+ *
+ * This file is part of Guintftp.
+ *
+ * Guintftp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Guintftp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Guintftp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
