35aa867bec294e2bce54bf9443991044d2a83859
dhernandez0 <dhernandez0@users.noreply.github.com>
2010-06-12 19:45:03 +0000

    oppss, license!

--- src/daemon/tftp_block_io.h
+++ src/daemon/tftp_block_io.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2010 Dani Hernández Juárez, dhernandez0@gmail.com
+ *
+ * This file is part of Guintftp.
+ *
+ * Guintftp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Guintftp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Guintftp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef TFTP_BLOCK_IO_H_
 #define TFTP_BLOCK_IO_H_
 
